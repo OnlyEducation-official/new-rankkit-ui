9e79dbcef0c6a22723cc195f4e037212f2fa9f32
Sarfraj <it.onlyeducation@gmail.com>
2025-07-09 17:30:49 +0530

    first commit

---
+++ src/components/svg/index.tsx
@@ -0,0 +1,10 @@
+import ProjectShape,{ShapeTwo} from "@/components/svg/project-shape";
+import RightArrow from "./right-arrow";
+import Leaf from "./leaf"
+
+
+export{
+    ShapeTwo,
+    RightArrow,
+    Leaf
+}
