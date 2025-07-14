e7ebb525e159ea83213a20f69be21ada461c1133
Sarfraj <it.onlyeducation@gmail.com>
2025-07-14 17:16:13 +0530

    porfolio data added

--- src/components/project/project-four.tsx
+++ src/components/project/project-four.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 import { ProjectShape, RightArrow } from "../svg";
 // images
-import port_1 from "../../../public/assets/img/home-03/portfolio/port-1.jpg";
-import port_2 from "../../../public/assets/img/home-03/portfolio/port-2.jpg";
+import port_1 from "../../../public/assets/img/home-03/portfolio/port-3.jpg";
+import port_2 from "../../../public/assets/img/portfolio-img/123123.svg";
 import port_3 from "../../../public/assets/img/home-03/portfolio/port-3.jpg";
 import port_4 from "../../../public/assets/img/home-03/portfolio/port-4.jpg";
 import port_5 from "../../../public/assets/img/home-03/portfolio/port-5.jpg";
-import port_6 from "../../../public/assets/img/home-03/portfolio/port-6.jpg";
+import port_6 from "../../../public/assets/img/portfolio-img/tktlogoas.png";
 import port_7 from "../../../public/assets/img/home-03/portfolio/port-7.jpg";
 import port_8 from "../../../public/assets/img/home-03/portfolio/port-8.jpg";
 import Link from "next/link";
@@ -45,39 +45,39 @@ const project_data = [
     img_2: port_8,
     meta: "SEP 2024 . Creative",
     title: "Only Engineering",
-    slug:"only-engineering"
+    slug: "only-engineering",
   },
-    {
+  {
     id: 5,
     img_1: port_7,
     img_2: port_8,
     meta: "SEP 2024 . Creative",
     title: "MBACET",
-    slug:"mba-cet"
+    slug: "mba-cet",
   },
-      {
+  {
     id: 6,
     img_1: port_7,
     img_2: port_8,
     meta: "SEP 2024 . Creative",
     title: "KALP",
-    slug:"kalp"
+    slug: "kalp",
   },
-        {
+  {
     id: 7,
     img_1: port_7,
     img_2: port_8,
     meta: "SEP 2024 . Creative",
     title: "Bake Ur Day",
-    slug:"bake-ur-day"
+    slug: "bake-ur-day",
   },
-          {
+  {
     id: 8,
     img_1: port_7,
     img_2: port_8,
     meta: "SEP 2024 . Creative",
     title: "Toy Room",
-    slug:"toy-room",
+    slug: "toy-room",
   },
 ];
 
@@ -87,7 +87,11 @@ type IProps = {
 };
 export default function ProjectFour({ style_2 = false }: IProps) {
   return (
-    <div className={`tp-project-3-area ${style_2 ? "pt-60 pw-project-style" : "pt-130 black-bg"}`}>
+    <div
+      className={`tp-project-3-area ${
+        style_2 ? "pt-60 pw-project-style" : "pt-130 black-bg"
+      }`}
+    >
       <div className="container container-1720">
         {!style_2 && (
           <div className="row justify-content-center">
@@ -119,11 +123,22 @@ export default function ProjectFour({ style_2 = false }: IProps) {
                 <div className="row">
                   <div className="col-xl-4 col-lg-4 col-md-6">
                     <div className="tp-project-3-thumb pro-img-1">
-                      <Image
+                      {/* <Image
                         src={item.img_1}
                         alt="port-img"
-                        style={{ height: "auto", width:"-webkit-fill-available" }}
-                      />
+                        style={{
+                          height: "auto",
+                          width: "-webkit-fill-available",
+                        }}
+                      /> */}
+                      <video
+                        src="/assets/img/portfolio-img/tkt reel.mp4"
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                        style={{ width: "100%", height: "auto", borderRadius:"20px", objectFit:"cover"}}
+                      ></video>
                     </div>
                   </div>
                   <div className="col-xl-4 col-lg-4 col-md-12 order-1 order-lg-0">
@@ -158,7 +173,6 @@ export default function ProjectFour({ style_2 = false }: IProps) {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
